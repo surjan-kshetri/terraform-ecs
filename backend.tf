f6100944ab8849084a0fcb4436ea53844c016e35
surjan-kshetri <kshe9881@stthomas.edu>
2020-02-27 17:26:22 -0500

    Added new files

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-surjan"
+    bucket = "terraform-surjan123"
     key    = "terraform/dev/ecs-ngnix"
     region = "us-east-1"
     profile = "surjan"
