ef53d853369ffd215b4ea603b13163a916915222
surjan-kshetri <kshe9881@stthomas.edu>
2020-02-28 16:13:17 -0500

    Added other files

---
+++ alb.tf
@@ -0,0 +1,23 @@
+resource "aws_lb" "default" {
+  name               = "terraform-ecs-nginx"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.default_lb.id}"]
+  subnets            = ["subnet-c7415ba0", "subnet-a873d9e5"]  enable_deletion_protection = false
+  tags = {
+    Environment = "dev"
+  }
+}resource "aws_lb_listener" "default" {
+  load_balancer_arn = "${aws_lb.default.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.default.arn}"
+  }
+}resource "aws_lb_target_group" "default" {
+  name     = "terraform-ecs-nginx"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "vpc-d86355a2"
+}
